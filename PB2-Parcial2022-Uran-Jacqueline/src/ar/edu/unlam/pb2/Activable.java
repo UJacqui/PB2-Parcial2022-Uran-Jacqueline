f8bc86a50ad69aded25365b801f4b70ce49496e6
UJacqui <102489620+UJacqui@users.noreply.github.com>
2022-11-09 17:35:36 -0300

    Update Activable.java

--- PB2-Parcial2022-Uran-Jacqueline/src/ar/edu/unlam/pb2/Activable.java
+++ PB2-Parcial2022-Uran-Jacqueline/src/ar/edu/unlam/pb2/Activable.java
@@ -1,5 +1,17 @@
 package ar.edu.unlam.pb2;
 
 public interface Activable {
+	
+	public boolean agregarAlarma(Alarma alarma, Central central);
+	
+	public boolean agregarUsuario(Usuario usuario);
+	
+	public void agregarUsuarioAUnaAlarma(Configurador configurador, Integer idAlarma, Integer codConfiguracion) throws CodigoAlarmaIncorrectoException;
+	
+	public boolean agregarSensorAAlarma(Alarma alarma, Sensor sensor, Integer dni) throws SensorDuplicadoException;
+	
+	public boolean activarSensorDeAlarma(Integer identificador, Integer idAlarma, CodActivacion cod );
+	
+	public boolean activarDesactivarAlarma(Alarma alarma, CodActivacion cod, Configurador usuario) throws SensorDesactivadoException;
 
 }
