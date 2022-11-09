27740f5e75b0b18f1f08f988591b34b43f9aabd8
UJacqui <102489620+UJacqui@users.noreply.github.com>
2022-11-09 17:36:26 -0300

    Update Administrador.java

--- PB2-Parcial2022-Uran-Jacqueline/src/ar/edu/unlam/pb2/Administrador.java
+++ PB2-Parcial2022-Uran-Jacqueline/src/ar/edu/unlam/pb2/Administrador.java
@@ -3,7 +3,7 @@ package ar.edu.unlam.pb2;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Administrador extends Usuario implements Configurable {
+public class Administrador extends Usuario implements Activable {
 	
 	private Set<Alarma> listaAlarma = new HashSet<>();
 	
@@ -11,13 +11,53 @@ public class Administrador extends Usuario implements Configurable {
 		super(dni, nombre);
 	}
 
-	public Boolean agregarAlarma(Alarma alarma, Central central) {
+	@Override
+	public boolean agregarAlarma(Alarma alarma, Central central) {
 		this.listaAlarma.add(alarma);
+		central.registrarAlarma(alarma);
 		return true;
 	}
 	
 	public Integer cantAlarmasRegistradas () {
 		return listaAlarma.size();
+	}
+
+
+	@Override
+	public boolean agregarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void agregarUsuarioAUnaAlarma(Configurador configurador, Integer idAlarma, Integer codConfiguracion) throws CodigoAlarmaIncorrectoException {
+		
+		for (Alarma alarma : listaAlarma) {
+			
+			if(alarma.getIdAlarma().equals(idAlarma)) {
+				alarma.agregarUsuario(configurador, codConfiguracion);
+			}
+		}
+		
+	}
+
+	@Override
+	public boolean agregarSensorAAlarma(Alarma alarma, Sensor sensor, Integer dni) throws SensorDuplicadoException {
+		alarma.agregarSensor(alarma, sensor, dni);
+		return true;
+	}
+
+	@Override
+	public boolean activarSensorDeAlarma(Integer identificador, Integer idAlarma, CodActivacion cod) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean activarDesactivarAlarma(Alarma alarma, CodActivacion cod, Configurador usuario) throws SensorDesactivadoException {
+		
+		alarma.activarAlarma(alarma, cod, usuario);
+		return false;
 	}
 	
 	
